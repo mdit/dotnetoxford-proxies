b2be9e7c1739ac8e27b910d31edf9d0a17266378
Matt Davidson <matt.davidson@williamsf1.com>
2023-06-02 23:21:08 +0100

    Adds command line demo app.

---
+++ DotNetOxford.ProxyDemo/DotNetOxford.ProxyDemo.CommandLine/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetOxford.ProxyDemo.Model;
+
+var manager = new EngineeringManager("Laid Back Larry");
+var lead = new LeadSoftwareEngineer("Meticulous Max");
+var snr = new SeniorSoftwareEngineer("Picky Johnny Coder");
+var jnr = new SoftwareEngineer("Little Bobby Tables");
+manager.DirectReports.AddRange(new[] { lead, snr, jnr });
+lead.TeamMates.AddRange(new[] { snr, jnr });
+
+var code = manager.RequestCodeFromTeam();
+Console.Write(code);
+Console.ReadKey();
