959a510be41fbd4891778755e9617c193a6ad944
Matt Davidson <matt.davidson@williamsf1.com>
2023-06-02 23:27:00 +0100

    Proxy instance demo.

--- DotNetOxford.ProxyDemo/DotNetOxford.ProxyDemo.CommandLine/Program.cs
+++ DotNetOxford.ProxyDemo/DotNetOxford.ProxyDemo.CommandLine/Program.cs
@@ -1,14 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using DotNetOxford.ProxyDemo;
 using DotNetOxford.ProxyDemo.Model;
 
 var manager = new EngineeringManager("Laid Back Larry");
 var lead = new LeadSoftwareEngineer("Meticulous Max");
 var snr = new SeniorSoftwareEngineer("Picky Johnny Coder");
 var jnr = new SoftwareEngineer("Little Bobby Tables");
+
 manager.DirectReports.AddRange(new[] { lead, snr, jnr });
 lead.TeamMates.AddRange(new[] { snr, jnr });
 
-var code = manager.RequestCodeFromTeam();
-Console.Write(code);
-Console.ReadKey();
+Console.WriteLine("Concrete instances");
+Console.WriteLine(manager.RequestCodeFromTeam());
+Console.ReadLine();
+
+// Proxies
+Console.WriteLine("Proxy instance");
+var proxyManager = new EngineeringManagerProxy(manager);
+Console.WriteLine(proxyManager.RequestCodeFromTeam());
+Console.ReadLine();
+
