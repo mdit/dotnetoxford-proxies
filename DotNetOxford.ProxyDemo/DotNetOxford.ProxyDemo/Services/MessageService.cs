e632e0a424ecd015ab2931daed4d4bcb386dd8b3
Matt Davidson <matt.davidson@williamsf1.com>
2023-06-05 21:27:37 +0100

    Adds services.

---
+++ DotNetOxford.ProxyDemo/DotNetOxford.ProxyDemo/Services/MessageService.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DotNetOxford.ProxyDemo.Services;
+
+public class MessageService
+{
+    private const uint MB_YESNO = 0x4;
+
+    public void DisplayMessageBox(string prompt, string title)
+    {
+        MessageBox(IntPtr.Zero, prompt, title, MB_YESNO);
+    }
+
+    [DllImport("user32.dll")]
+    private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+}
---
+++ DotNetOxford.ProxyDemo/DotNetOxford.ProxyDemo/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using DotNetOxford.ProxyDemo.Model;
+
+namespace DotNetOxford.ProxyDemo.Services
+{
+    public class UserService
+    {
+        private readonly MessageService _messageService;
+
+        public UserService(MessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public void DisplayUserMessage(DevTeamMember member, string message)
+        {
+            _messageService.DisplayMessageBox(
+                $"{member.Name} ({member.RoleName}): {message}",
+                "Dev Team Member message");
+        }
+    }
+}
